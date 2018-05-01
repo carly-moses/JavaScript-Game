52a9d1e127f9acacefe1304d90454cc232299b58
Carly Moses <cmoses916@Yahoo.com>
2018-05-01 14:57:25 -0400

    Java Script Game Part 1

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+let greeting = prompt ("Would you like to play the game?");
+
+if (greeting.toLowerCase() === "yes") {
+    let userHealth = 40; 
+    let grantHealth = 10; 
+    let userName = prompt ("What's your character's name?");
+    let wins = 0; 
+    while (grantHealth > 0) {
+        console.log (`${userName}'s Health: ${userHealth}`);
+        console.log (`Grant's Health: ${grantHealth}`);
+        console.log (`${userName}'s Health: ${userHealth -= Math.floor(Math.random() * 2+1)}`);
+        console.log (`Grant's Health: ${grantHealth -= Math.floor(Math.random() * 2+1)}`);
+        if (wins === 3) {
+            console.log (`Congrats ${userName}!`); 
+            break;
+        } else if (userHealth <= 0) {
+            console.log (`Game Over - ${userName} loses, Grant wins!"`); 
+            break; 
+        } else if (grantHealth <= 0) {
+            wins++; 
+            console.log (`${userName} wins: ${wins}`);
+            grantHealth = 10; 
+        }
+    } 
+} else if (greeting === "no") {
+    console.log ("Bummer. Maybe next time!");
+} 
+
